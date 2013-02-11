fec709f221f14b3761ad1a5c34af9f4b7cf277d9
oropon <momochitose@gmail.com>
2013-02-11 15:58:42 +0900

    Initial Commit

---
+++ UnderscoreFreak/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  UnderscoreFreak
+//
+//  Created by Shogo on 2013/02/11.
+//  Copyright (c) 2013年 oropon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
